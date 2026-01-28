a576ba5f0a4f20deceebbfc2f69ab8d07d1bb3eb
saladve <sachi.akutsu0111@gmail.com>
2026-01-28 15:33:02 +0900

    1/28v1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./index.jsx",
+    "./app.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
